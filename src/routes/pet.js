df091afb4d6a4f29449ce065481b39979616f90c
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-05 12:43:26 -0500

    Create pet route

---
+++ src/routes/pet.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const petController = require('../controllers/pet.controller');
+
+router.route('/').get(petController.list);
+router.route('/').post(petController.create);
+router.route('/:userID').put(petController.update);
+router.route('/:userID').delete(petController.destroy);
+
+module.exports = router;
