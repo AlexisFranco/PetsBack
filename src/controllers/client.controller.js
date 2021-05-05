ad4ed575635b5f8b58122ff64a34b3beda1b76f2
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-04 20:14:35 -0500

    Create client controller

---
+++ src/controllers/client.controller.js
@@ -0,0 +1,59 @@
+const jwt = require('jsonwebtoken');
+const Client = require('../models/client.model');
+
+module.exports = {
+  async signup(req, res) {
+    try {
+      const { body } = req;
+
+      const client = await Client.create(body);
+      const token = jwt.sign(
+        { userID: client._id, userType: 'client' },
+        process.env.SECRET,
+        { expiresIn: 60 * 60 * 24 }
+      );
+      res
+        .status(200)
+        .json({ message: 'Client created successfully', token, client });
+    } catch (error) {
+      res.status(400).json({ message: 'Client could not be created', error });
+    }
+  },
+  async list(req, res) {
+    try {
+      const { query } = req;
+
+      const clients = await Client.find(query);
+      res
+        .status(200)
+        .json({ message: `${clients.length} clients found`, clients });
+    } catch (error) {
+      res.status(400).json({ message: 'Clients could not be found', error });
+    }
+  },
+  async update(req, res) {
+    try {
+      const {
+        body,
+        params: { userID },
+      } = req;
+
+      const clientUpdate = await Client.findByIdAndUpdate(userID, body, {
+        new: true,
+      });
+      res.status(200).json({ message: 'Client updated', clientUpdate });
+    } catch (error) {
+      res.status(400).json({ message: 'Client could not be updated', error });
+    }
+  },
+  async destroy(req, res) {
+    try {
+      const { userID } = req.params;
+
+      const clientDelete = await Client.findByIdAndDelete(userID);
+      res.status(200).json({ message: 'Client deleted', clientDelete });
+    } catch (error) {
+      res.status(400).json({ message: 'Client could not be deleted', error });
+    }
+  },
+};
