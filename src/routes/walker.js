6742b4c90baa3bfba358a42fa8302bda53138ccb
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-06 08:02:56 -0500

    Add auth middleware in walker MVC

--- src/routes/walker.js
+++ src/routes/walker.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const walkerController = require('../controllers/walker.controller');
+const { auth } = require('../utils/auth');
 
 router.route('/').get(walkerController.list);
 router.route('/').post(walkerController.signup);
-router.route('/:userID').put(walkerController.update);
-router.route('/:userID').delete(walkerController.destroy);
+router.route('/').put(auth, walkerController.update);
+router.route('/').delete(auth, walkerController.destroy);
 
 module.exports = router;
