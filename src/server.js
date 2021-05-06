dd9f8709194cc33596a69c1900cef21de8556847
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-05 20:26:38 -0500

    Add zones routes in server.js

--- src/server.js
+++ src/server.js
@@ -4,11 +4,12 @@ const morgan = require('morgan');
 const express = require('express');
 const { connect } = require('./db');
 
+const petRouter = require('./routes/pet');
+const zoneRouter = require('./routes/zone');
 const clientRouter = require('./routes/client');
 const walkerRouter =  require('./routes/walker');
-const petRouter = require('./routes/pet');
-const medicineRouter = require('./routes/medicine');
 const serviceRouter = require('./routes/service');
+const medicineRouter = require('./routes/medicine');
 const typeMedicationRouter = require('./routes/typeMedication');
 
 const port = process.env.PORT;
@@ -19,11 +20,12 @@ app.use(express.json());
 app.use(cors({ origin: '*' }));
 app.use(morgan('dev'));
 
+app.use('/pets', petRouter);
+app.use('/zones', zoneRouter);
 app.use('/clients', clientRouter);
 app.use('/walkers', walkerRouter);
-app.use('/pets', petRouter);
-app.use('/medicines', medicineRouter);
 app.use('/services', serviceRouter);
+app.use('/medicines', medicineRouter);
 app.use('/types', typeMedicationRouter);
 
 app.listen(port, () => {
