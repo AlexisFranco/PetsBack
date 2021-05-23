977a5d866112f14f1cf2866267849bc523bd4510
Alexis Franco <alexisfrancolopez@gmail.com>
2021-05-23 16:29:41 -0500

    Change MVC pet for update pet with their photos
    * Create zone model
    
    * Changes in controllers and models logic
    
    * Fix MVC client
    
    * Changes in controllers
    
    * Deleted populate in client controller in the show method
    
    * Deleted validation with only medicine name in medicines
    
    * Add populate en walker list
    
    * Change list and update controller service and walker
    
    * Add walkerID populate in list service
    
    * Add middleware formData in MVC walker
    
    * Change in MVC pets to update photo

--- src/controllers/pet.controller.js
+++ src/controllers/pet.controller.js
@@ -6,7 +6,7 @@ module.exports = {
     try {
       const { body, userID, userType } = req;
 
-      if (userType === 'client'){
+      if (userType === 'client') {
         const pet = await Pet.create({ ...body, clientID: userID });
         const client = await Client.findById(userID);
 
@@ -14,9 +14,8 @@ module.exports = {
         await client.save({ validateBeforeSave: false });
         res.status(200).json({ message: 'Pet created successfully', pet });
       } else {
-        throw Error ('User was not client')
+        throw Error('User was not client');
       }
-
     } catch (error) {
       res.status(400).json({ message: error.message });
     }
@@ -25,13 +24,26 @@ module.exports = {
     try {
       const { userID } = req;
       const objClient = {
-        'clientID': userID,
-      }
+        clientID: userID,
+      };
       const pets = await Pet.find(objClient).populate('medicineIDs');
       res.status(200).json({ message: `${pets.length} pets found`, pets });
-
     } catch (error) {
       res.status(400).json({ message: 'Pets could not be found', error });
+    }
+  },
+  async show(req, res) {
+    try {
+      const { userID, params: { petID } } = req;
+      const pet = await Pet.findById(petID);
+
+      if (pet.clientID.toString() === userID.toString()) {
+        res.status(200).json({ message: 'Pet found', pet });
+      } else {
+        throw 'Pet is not owned by current client';
+      }
+    } catch (error) {
+      res.status(400).json({ message: 'Pet could not be found', error });
     }
   },
   async update(req, res) {
@@ -40,13 +52,14 @@ module.exports = {
       const petID = body.petID;
       const pet = await Pet.findById(petID);
 
-      if(pet.clientID.toString() === userID.toString()) {
-        const petUpdate = await Pet.findByIdAndUpdate(petID, body, { new: true });
+      if (pet.clientID.toString() === userID.toString()) {
+        const petUpdate = await Pet.findByIdAndUpdate(petID, body, {
+          new: true,
+        });
         res.status(200).json({ message: 'Pet updated', petUpdate });
       } else {
-        throw 'Pet is not owned by current client'
+        throw 'Pet is not owned by current client';
       }
-
     } catch (error) {
       res.status(400).json({ message: 'Pet could not be updated', error });
     }
@@ -64,9 +77,8 @@ module.exports = {
         const petDelete = await Pet.findByIdAndDelete(petID);
         res.status(200).json({ message: 'Pet deleted', petDelete });
       } else {
-        throw 'Pet not owned'
+        throw 'Pet not owned';
       }
-
     } catch (error) {
       res.status(400).json({ message: 'Pet could not be deleted', error });
     }
--- src/routes/pet.js
+++ src/routes/pet.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const petController = require('../controllers/pet.controller');
 const { auth } = require('../utils/auth');
+const { formData } = require('../utils/formData');
 
 router.route('/').get(auth, petController.list);
+router.route('/:petID').get(auth, petController.show);
 router.route('/').post(auth, petController.create);
-router.route('/').put(auth, petController.update);
+router.route('/').put(auth, formData, petController.update);
 router.route('/').delete(auth, petController.destroy);
 
 module.exports = router;
